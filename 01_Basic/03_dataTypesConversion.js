68051c761e862b765b249cc27794657f188cad8a
nallabellisriparthuu <nallabellisriparthu@gmail.com>
2025-08-18 10:19:39 +0000

    added datatype conversion

---
+++ 01_Basic/03_dataTypesConversion.js
@@ -0,0 +1,152 @@
+// Data type conversion (Typecasting) in JavaScript.
+// -------------------------------------------------
+
+/*
+-> Javascript allow conversion between data-types. They are 2 types. Implicit typecasting and explicit type casting.
+*/
+
+/*
+-> Implicit typecasting: Implicitly typecasting covert values automatic depending upon the context.
+*/
+let num1 = "10";
+let num2 = 2;
+let num3 = 5
+console.log(num1 + num2); //102
+console.log(num3 + num1); //510
+console.log(num1 + num1); //1010
+console.log(num1 - num3); //5
+console.log(num2 * num1); //20
+console.log(true + num3); //6
+console.log(false + num2); //2
+console.log(num1 / num2); //5
+
+/*
+Note:
+-----
+    * Here we understand that '+' operator is string concatination that joins to aleat or both string value. Other operator are (-, *, /) are conver to the number and return math values
+*/
+
+console.log("************************************************************");
+
+/*
+-> Explicit typecasting: Explicit type castings are done manually like string to number, number to string, boolean to number. string to boolean. And more
+*/
+
+//string -> number
+let userName = "sai";
+console.log(typeof userName); //string
+//declaring varibales and using 'Number()' method to convert (S->N)
+let userNameToNumber = Number(userName); 
+console.log(typeof userNameToNumber); //number
+
+//Another way to S->N using 'parseInt()' & 'pareFloat()' method
+
+//ParseInt()
+let userId = "75243";
+console.log(userId); //75243
+console.log(typeof userId); //string
+let userId1 = parseInt(userId);
+console.log(userId1); //75243
+console.log(typeof userId1); //number
+
+//parseFloat()
+let price = "55.6";
+console.log(price); //55.6
+console.log(typeof price); //string
+let price1 = parseFloat(price);
+console.log(price1); //55.6
+console.log(typeof price1); //number
+
+//previous above method another example
+let age = "100";
+console.log(age); //100
+console.log(typeof age); //string
+//declaring varibales and using 'Number()' method to convert (S->N)
+let age1 = Number(age);
+console.log(age1); //100
+console.log(typeof age1); //number
+
+//number -> string are two types 'String()', '.toString()'
+
+//String()
+let paper = 256;
+console.log(paper); //256
+console.log(typeof paper); //number
+let paper1 = String(paper);
+console.log(paper); //256
+console.log(typeof paper1); //string
+
+//.toString()
+let emiId = 495672136423;
+console.log(emiId); //495672136423
+console.log(typeof emiId); //number
+let emiId1 = emiId.toString();
+console.log(emiId1); //495672136423
+console.log(typeof emiId1); //string
+
+console.log("************************************************************");
+
+let isFollow = 0;
+console.log(typeof isFollow); //number
+let isFollow1 = Boolean(isFollow);
+console.log(isFollow1); //false
+
+let isHave = 1;
+console.log(typeof isHave); //number
+let isHave1 = Boolean(isHave);
+console.log(typeof isHave1); //boolean (true)
+
+let isEmpty = '';
+console.log(typeof isEmpty); //string
+let isEmpty1 = Boolean(isEmpty);
+console.log(isEmpty1); //false
+
+let numbers = [1, 2];
+console.log(typeof numbers); //object
+let numbers1 = Boolean(numbers);
+console.log(typeof numbers1); //boolean (true)
+
+let score = {
+    win: 25,
+    lose: 2
+}
+console.log(typeof score); //object
+let score1 = Boolean({score})
+console.log(typeof score1); //boolean (true)
+
+/*
+Note: 
+-----    
+    * The values which showes 0, '', null, NaN, undefined, false are called falsy value.
+    * The values which is non-empty string, numbers other than 0, objects, arrays and all other vaules are truthy value
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
