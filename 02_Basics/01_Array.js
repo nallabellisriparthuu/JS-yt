0cc166395f22e18aec506953cf2d43a0226ce5eb
nallabellisriparthuu <nallabellisriparthu@gmail.com>
2025-08-19 16:08:10 +0000

    array written

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,8 @@
+/*
+
+-> Array: Array is the special type of object used to store multiple element in a single variable. It access element like string number, boolean, objects, arrays. It follows the ordered and start from the zero index.
+
+*/
+
+let alphaNumeric = [1, 2, 3, 4, true, false, {name: "sai"}, [5, 6, 7, 8]]
+console.log(alphaNumeric);
