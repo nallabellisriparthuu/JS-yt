e55d1ddeeefe07ea7ad8a7e7c5c8851af9b2118b
nallabellisriparthuu <nallabellisriparthu@gmail.com>
2025-09-01 10:02:59 +0000

    written about for loop

---
+++ 05_loops/iteration.js
@@ -0,0 +1,25 @@
+/*
+What is loop
+-> Loop is used to repete the code step by step with wriing the duplicate the code which is helpful for array, object, API's, data-handling. We have some of loops like for loop, while loop and do while loop.
+*/
+
+/*
+for loop: for loop is used when the number of iteration is know.
+
+syntax:
+-------
+for(initilization; condition; increment/decrement){
+    ...code...
+}
+*/
+
+for(let i = 1; i <= 10; i++){
+    let result1 = i;
+    console.log(result1);
+}
+
+/*
+-> initilization: Typically variable will be initilizes and statement will be executed onces before the loop start
+-> condition: This expression is evaluate before the each iteration start if the code is true loop is continue and if it is false loop will stop
+-> increment/decrement: This is usallay incerment and decrement the variable when the statement is executed after the each iteration
+*/
