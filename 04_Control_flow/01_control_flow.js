a525fd319d7ea3fd082278c47c48e36938ad2b68
nallabellisriparthuu <nallabellisriparthu@gmail.com>
2025-09-01 06:21:11 +0000

    writeen about if, ifelse and elseif and logical operator and combined both logical and conditional statement

--- 04_Control_flow/01_control_flow.js
+++ 04_Control_flow/01_control_flow.js
@@ -25,7 +25,7 @@ Logical operator
 
 Conditional statement
 ---------------------
-if, if...else, if...elseif & Switch
+if, if...else, if...else if & Switch
 
 Ternary Operator
 ----------------
@@ -66,9 +66,115 @@ if(isUnFollow){
 let name = "JavaScript";
 console.log(name); 
 
-//if...else: (if...else) condition block is execute when the if block of code is true, if it is false else block of code will execute
+//if...else:- (if...else) condition block is execute when the if block of code is true, if it is false else block of code will execute
 
 //Lets check the person is eligible to vote or not
-let personAge = 25;
+let personAge = 25; 
 let eligibleAgeToVote = 18;
-if()
+if(personAge > eligibleAgeToVote){
+    let result = "Vote to right";
+    console.log(result);
+}else{
+    let result1 = "Not vote to right";
+    console.log(result1);
+}
+
+// console.log(result);
+// console.log(result1);
+
+
+/*
+-> Change person age to below 18 and if you check result it show else block
+
+Scope reminder
+--------------
+-> If you log 'result' and 'result1' outside the block it shows error that 'ReferenceError: result is not defined' & 'ReferenceError: result1 is not defined
+
+-> Because 'let' & 'const' are the execute in the block of code where as 'var' is global/function scope it execute outside and inside the block.
+*/
+
+//if...else if:- (if...else if) is condition block it checkes the multiple block of code that which block of code is true that will be executed.
+
+//Lets check the final score of student Grade A to Fail
+let studentFinalScore = 50;
+
+//Here used Logical 'AND(&&)' operator that tells that GTE 90 is True and LTE 100 is True then only execute. Will understand this cleraly in Logical operator topic. 
+if(studentFinalScore >= 90 && studentFinalScore <=100){
+    console.log("Grade A");
+}else if(studentFinalScore >= 80){
+    console.log("Grade B");
+}else if(studentFinalScore >= 70){
+    console.log("Grade C");
+}else if(studentFinalScore >= 60){
+    console.log("Grade D");
+}else if(studentFinalScore >= 50){
+    console.log("Grade E");
+}else{
+    console.log("Fail");
+}
+
+/*
+Logical Operator
+----------------
+-> Logical operator is used to combine two or more conditions/expressions and return a single boolean value (true or false): They are AND(&&), OR(||), Not(!).
+
+-> AND(&&): The AND(&&) operartor used when if all the condition is only true return the true value, otherwise return false.
+    T - T = T
+    T - F = F
+    F - T = F
+    F - F = F
+
+-> OR(||): The OR(||) operator is used when any one of the condition is true return ture value, otherwise return false.
+    T - T = T
+    T - F = T
+    F - T = T
+    F - F = F
+
+-> Not(!): The Not(!) operator is used when the true condition is true return false and vise-versa
+    T = F
+    F = T
+*/
+
+//AND(&&)
+
+//Lets check the marks of the students that student all marks is above passing marks eligible to engineer otherwise not.
+let mathsMarks = 60;
+let passingMathsMarks = 40;
+let physicMarks = 55;
+let passingPhysicMarks = 35;
+let chemistryMarks = 35;
+let passingChemistryMarks = 35;
+
+if(mathsMarks >= passingMathsMarks && physicMarks >= passingPhysicMarks && chemistryMarks >= passingChemistryMarks){
+    console.log("eligible to engineer");
+}else{
+    console.log("Not eligible to engineer");
+}
+
+//-> Here all the condition is true such that true condition will be execute. If any one of AND should have false then the false condition will be execute
+
+//OR(||)
+
+//Lets check the marks of the students that student pass atleast one subject and eligible to engineer otherwise not.
+let mathsMarks1 = 60;
+let passingMathsMarks1 = 30;
+let physicMarks2 = 55;
+let passingPhysicMarks2 = 35;
+let chemistryMarks3 = 35;
+let passingChemistryMarks3 = 15;
+
+if(mathsMarks1 >= passingMathsMarks1 || physicMarks2 >= passingPhysicMarks2 || chemistryMarks3 >= passingChemistryMarks3){
+    console.log("eligible to engineer");
+}else{
+    console.log("Not eligible to engineer");
+}
+
+//Not(!)
+
+//Lets make the absent student present
+let isStudentPresent = false;
+if(!isStudentPresent){
+    console.log("Student present");
+}else{
+    console.log("Student absent");
+}
